d8a63afe693afc5a2d4e4c84f83fd166b1550d11
alldone <aldo.panetta@gmail.com>
2017-12-20 16:41:17 +0100

    Added FileWatcher to move...
    Added FileWatcher to move downloaded file in the right place .... when download in the watched/configured folder is completed

---
+++ WorkTools/Code/Model/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTools.Code.Model {
+    public class Configuration {
+        public string PathToWatch { get; set; }
+        public TimeSpan HourAlert1 { get; set; }
+        public TimeSpan HourAlert2 { get; set; }
+        public string LastFolder { get; set; }
+    }
+}
